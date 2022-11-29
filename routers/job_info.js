825f2227f3cd5bbc9795440642c65094d84e8f3c
Kenan <kenanbaylan5434@gmail.com>
2022-11-29 22:59:07 +0300

    database added and restapi

---
+++ routers/job_info.js
@@ -0,0 +1,13 @@
+// import express from "express";
+// import postgresClient from "./config/database.js";
+
+// const router = express.Router();
+
+// //create address ;
+// router.post("/job_info", async (request, response) => {
+//     try {
+//         const text ="INSERT INTO job_info (jobName,jobDesc,) VALUES ($1,$2,$3) RETURNING *";
+
+//     } catch (error) {
+
+//     }
